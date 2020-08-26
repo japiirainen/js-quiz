26a04a2a14198d79bda2f3f918ef32d4bdb0c5d0
Joona Piirainen <joona.piirainen@gmail.com>
2020-08-26 12:43:06 +0300

    test

--- server/src/test/cli.test.ts
+++ server/src/test/cli.test.ts
@@ -1,3 +1,9 @@
+import { runCLI } from 'jest'
+import path from 'path'
+
+const projectRootPath = path.join(__dirname, '..')
+console.log(projectRootPath)
+
 const userCode = `function add(a, b) { return a + b }`
 const problemCalls = [`add(2, 2)`, `add(4, 100)`, `add(0, 5)`].map(call => {
    return eval(`${userCode}${call}`)
@@ -5,10 +11,8 @@ const problemCalls = [`add(2, 2)`, `add(4, 100)`, `add(0, 5)`].map(call => {
 const results = ['4', '104', '5'].map(x => Number(x))
 test('should add 2 numbers together', () => {
    problemCalls.map(call => {
-      results.map(results => {
-         ;() => {
-            expect(call).toEqual(results)
-         }
+      results.map(results => () => {
+         expect(call).toEqual(results)
       })
    })
 })
