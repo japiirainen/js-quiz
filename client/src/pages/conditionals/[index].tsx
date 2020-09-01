445acd5a5fc2223c55f9e3c22f0ac484513156f9
Joona Piirainen <joona.piirainen@gmail.com>
2020-09-01 20:12:10 +0300

    inc+ sub

--- client/src/pages/conditionals/[index].tsx
+++ client/src/pages/conditionals/[index].tsx
@@ -10,6 +10,10 @@ import { ChallengeDesc } from '../../components/ChallengeDesc'
 import { Challenge } from '../../components/Challenge'
 import { Button, Flex, Text } from '@chakra-ui/core'
 import NextLink from 'next/link'
+import { add, subtract, __ } from 'ramda'
+
+const inc = add(1)
+const sub = subtract(__, 1)
 
 const Conditionals: NextPage = () => {
    const router = useRouter()
@@ -21,10 +25,11 @@ const Conditionals: NextPage = () => {
    const findProblemWithIndex = (queryIndex: number) => {
       return data?.findProblemsInGroup?.filter(problem => problem?.index === queryIndex)[0]
    }
-
+   console.log(sub(routeIndex))
+   console.log(inc(routeIndex))
    const problem = findProblemWithIndex(routeIndex)
-   const nextProblem = findProblemWithIndex(routeIndex + 1)
-   const prevProblem = findProblemWithIndex(routeIndex - 1)
+   const nextProblem = findProblemWithIndex(inc(routeIndex))
+   const prevProblem = findProblemWithIndex(sub(routeIndex))
 
    return (
       <Layout
@@ -38,27 +43,19 @@ const Conditionals: NextPage = () => {
          <Challenge problemData={problem} error={error} loading={fetching} />
          <Flex mt={15}>
             {nextProblem && (
-               <NextLink href={`/conditionals/${routeIndex + 1}`}>
-               <Button
-                  ml={'auto'}
-                  variant="solid"
-                  variantColor="blue"
-               >
-                  <Text mr={4}>next:</Text>
-                  <Text>({nextProblem.name})</Text>
-               </Button>
+               <NextLink href={`/conditionals/${inc(routeIndex)}`}>
+                  <Button ml={'auto'} variant="solid" variantColor="blue">
+                     <Text mr={4}>next:</Text>
+                     <Text>({nextProblem.name})</Text>
+                  </Button>
                </NextLink>
             )}
             {prevProblem && (
-               <NextLink href={`/conditionals/${routeIndex - 1}`}>
-               <Button
-                  mr={'auto'}
-                  variant="solid"
-                  variantColor="pink"
-               >
-                  <Text mr={4}>Prev:</Text>
-                  <Text>({prevProblem.name})</Text>
-               </Button>
+               <NextLink href={`/conditionals/${sub(routeIndex)}`}>
+                  <Button mr={'auto'} variant="solid" variantColor="pink">
+                     <Text mr={4}>Prev:</Text>
+                     <Text>({prevProblem.name})</Text>
+                  </Button>
                </NextLink>
             )}
          </Flex>
