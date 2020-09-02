a96a5f1a42969106976cbd98ecf4b6bf7049b74d
Joona Piirainen <joona.piirainen@gmail.com>
2020-09-02 21:18:41 +0300

    fix types

--- client/src/utils/helperFns.ts
+++ client/src/utils/helperFns.ts
@@ -1,4 +1,5 @@
 import { add, subtract, __, multiply, compose, divide, length } from 'ramda'
+import { Maybe } from 'graphql/jsutils/Maybe'
 
 export const inc = add(1)
 
@@ -8,7 +9,7 @@ export const multBy100 = multiply(100)
 
 export const calcPercentage = compose(Math.round, multBy100, divide(__))
 
-export const filterIds = (userIds: any, groupIds: any) =>
-   groupIds.filter((x: any) => userIds.includes(x))
+export const filterIds = (userIds: Maybe<string>[], groupIds: string[]) =>
+   groupIds.filter((x: Maybe<string>) => userIds.includes(x))
 
 export const calcAmount = compose(length, filterIds)
