90cd160074bd79b2a29445f501612f999f1b061b
Joona Piirainen <joona.piirainen@gmail.com>
2020-09-02 15:33:30 +0300

    progress page

--- client/src/pages/profile/progress.tsx
+++ client/src/pages/profile/progress.tsx
@@ -10,22 +10,63 @@ import {
    StatHelpText,
    StatArrow,
    Heading,
+   Flex,
 } from '@chakra-ui/core'
-import { useGetAllProblemsQuery, useMeQuery } from '../../generated/graphql'
+import {
+   useGetAllProblemsQuery,
+   useMeQuery,
+   useGetProblemsInGroupQuery,
+} from '../../generated/graphql'
 import { isServer } from '../../utils/isServer'
+import { LoadingSpinner } from '../../components/LoadingSpinner'
 
 const Progress = () => {
-   const [{ data: problemData }] = useGetAllProblemsQuery({ pause: isServer() })
+   const [{ data: problemData, fetching: problemFetching }] = useGetAllProblemsQuery({
+      pause: isServer(),
+   })
    const [{ data: meData }] = useMeQuery({ pause: isServer() })
+   const [{ data: basicsData, fetching: basicsFetching }] = useGetProblemsInGroupQuery({
+      variables: { groupName: 'basics' },
+      pause: isServer(),
+   })
+   const [{ data: condData, fetching: condFetching }] = useGetProblemsInGroupQuery({
+      variables: { groupName: 'conditionals' },
+      pause: isServer(),
+   })
 
    const allProblemsLen = problemData?.getAllProblems.length && length(problemData?.getAllProblems)
-   const userProblemsLen = meData?.me?.completedProblems && length(meData.me.completedProblems)
-
+   const userAllProblemsLen = meData?.me?.completedProblems && length(meData.me.completedProblems)
+   const basicProblemsLen =
+      basicsData?.findProblemsInGroup?.length && length(basicsData?.findProblemsInGroup)
+   const condProblemsLen =
+      condData?.findProblemsInGroup?.length && length(condData?.findProblemsInGroup)
+   const userBasicsProblemsLen =
+      basicsData?.findProblemsInGroup &&
+      length(
+         basicsData?.findProblemsInGroup.filter(
+            x => x && meData?.me?.completedProblems?.includes(x?._id)
+         )
+      )
+   const userCondProblemsLen =
+      condData?.findProblemsInGroup &&
+      length(
+         condData?.findProblemsInGroup.filter(
+            x => x && meData?.me?.completedProblems?.includes(x?._id)
+         )
+      )
    const allPercentage =
-      userProblemsLen &&
+      userAllProblemsLen &&
       allProblemsLen &&
-      Math.round(multiply(divide(userProblemsLen, allProblemsLen), 100))
+      Math.round(multiply(divide(userAllProblemsLen, allProblemsLen), 100))
 
+   const basicsPercentage =
+      userBasicsProblemsLen &&
+      basicProblemsLen &&
+      Math.round(multiply(divide(userBasicsProblemsLen, basicProblemsLen), 100))
+   const condPercentage =
+      userCondProblemsLen &&
+      condProblemsLen &&
+      Math.round(multiply(divide(userCondProblemsLen, condProblemsLen), 100))
    return (
       <AccountLayout
          bc2Text={'settings'}
@@ -38,26 +79,57 @@ const Progress = () => {
          variant={'small'}
          minH={'100vh'}
       >
-         <Heading>Progress in challenges</Heading>
-         <StatGroup>
-            <Stat>
-               <StatLabel>Total completed:</StatLabel>
-               <StatNumber>
-                  {userProblemsLen} / {allProblemsLen}
-               </StatNumber>
-               <StatHelpText>
-                  <StatArrow type="increase" />
-                  {allPercentage} %
-               </StatHelpText>
+         <Heading textAlign="center" fontSize={30}>
+            Progress in challenges
+         </Heading>
+         <StatGroup direction="column" alignItems={'center'}>
+            <Stat mt={8}>
+               <StatLabel fontSize={20}>Total completed:</StatLabel>
+               {problemFetching ? (
+                  <LoadingSpinner />
+               ) : (
+                  <Flex direction="column" alignItems="center">
+                     <StatNumber>
+                        {userAllProblemsLen} / {allProblemsLen}
+                     </StatNumber>
+                     <StatHelpText>
+                        <StatArrow type={userAllProblemsLen === 0 ? 'decrease' : 'increase'} />
+                        {allPercentage} %
+                     </StatHelpText>
+                  </Flex>
+               )}
             </Stat>
-
-            <Stat>
-               <StatLabel>Clicked</StatLabel>
-               <StatNumber>45</StatNumber>
-               <StatHelpText>
-                  <StatArrow type="decrease" />
-                  9.05%
-               </StatHelpText>
+            <Stat mt={8}>
+               <StatLabel fontSize={20}>Basics completed:</StatLabel>
+               {basicsFetching ? (
+                  <LoadingSpinner />
+               ) : (
+                  <Flex direction="column" alignItems="center">
+                     <StatNumber>
+                        {userBasicsProblemsLen} / {basicProblemsLen}
+                     </StatNumber>
+                     <StatHelpText>
+                        <StatArrow type={userBasicsProblemsLen === 0 ? 'decrease' : 'increase'} />
+                        {basicsPercentage} %
+                     </StatHelpText>
+                  </Flex>
+               )}
+            </Stat>
+            <Stat mt={8}>
+               <StatLabel fontSize={20}>Conditionals completed:</StatLabel>
+               {condFetching ? (
+                  <LoadingSpinner />
+               ) : (
+                  <Flex direction="column" alignItems="center">
+                     <StatNumber>
+                        {userCondProblemsLen} / {condProblemsLen}
+                     </StatNumber>
+                     <StatHelpText>
+                        <StatArrow type={userCondProblemsLen === 0 ? 'decrease' : 'increase'} />
+                        {condPercentage} %
+                     </StatHelpText>
+                  </Flex>
+               )}
             </Stat>
          </StatGroup>
       </AccountLayout>
