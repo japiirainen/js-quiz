0a6926603c1f16f1fc50679973a6b9be954411cb
Joona Piirainen <joona.piirainen@gmail.com>
2020-09-03 12:52:58 +0300

    fix test

--- client/src/utils/helperFns.test.ts
+++ client/src/utils/helperFns.test.ts
@@ -1,6 +1,6 @@
 import { calcAmount } from './helperFns'
-export const userProblemIds = [1, 3, 5]
-export const basicsIds = [1, 2, 3, 4, 5]
+export const userProblemIds = ['1', '3', '5']
+export const basicsIds = ['1', '2', '3', '4', '5']
 test('it should return the number of problems completed by the user in a group', () => {
    const res = calcAmount(userProblemIds, basicsIds)
    expect(res).toBe(3)
