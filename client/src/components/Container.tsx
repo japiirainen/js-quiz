90b9f42b35b8f4d0c74de58b00f805fe6c25f122
Joona Piirainen <joona.piirainen@gmail.com>
2020-09-20 19:27:53 +0300

    remove bgcolor bug

--- client/src/components/Container.tsx
+++ client/src/components/Container.tsx
@@ -1,4 +1,4 @@
-import { Flex, useColorMode, Box } from '@chakra-ui/core'
+import { Flex, Box } from '@chakra-ui/core'
 import React from 'react'
 
 export type ContainerVariant = 'small' | 'regular'
@@ -6,20 +6,8 @@ export type ContainerVariant = 'small' | 'regular'
 type WrapperProps = any
 
 export const Container: React.FC<WrapperProps> = ({ children, variant = 'regular', ...rest }) => {
-   const { colorMode } = useColorMode()
-
-   const bgColor = { light: 'gray.50', dark: 'gray.900' }
-   const color = { light: 'black', dark: 'white' }
-
    return (
-      <Flex
-         direction="column"
-         alignItems="center"
-         justifyContent="flex-start"
-         bg={bgColor[colorMode]}
-         color={color[colorMode]}
-         {...rest}
-      >
+      <Flex direction="column" alignItems="center" justifyContent="flex-start" {...rest}>
          <Box mt={8} mx="auto" maxW={variant === 'regular' ? '800px' : '400px'} w="100%">
             {children}
          </Box>
