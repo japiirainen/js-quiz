772821b2ea310e80ffd69e63f60835ca4af8d974
japiirainen <joona.piirainen@gmail.com>
2020-12-17 07:42:43 +0200

    push to dh with script

---
+++ push-to-dockerhub.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo server tag?
+read SERVER_VERSION
+echo SERVER_VERSION
+
+
+docker build -t japiirainen/js-quiz:$SERVER_VERSION ./server
+docker push japiirainen/js-quiz:$SERVER_VERSION
+
+echo client version?
+read CLIENT_VERSION
+echo CLIENT_VERSION
+
+docker build -t japiirainen/js-quiz-client:$CLIENT_VERSION ./client
+docker push japiirainen/js-quiz-client:$CLIENT_VERSION
